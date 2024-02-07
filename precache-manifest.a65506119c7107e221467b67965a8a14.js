476f372eafce46d3f3b8c78d3804ef952b227d51
Abhay Banugariya <abhaybanugariya09@gmail.com>
2024-02-07 22:58:05 +0530

    Updates

---
+++ precache-manifest.a65506119c7107e221467b67965a8a14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66d2195c72367b2d49528b69f6f013a",
+    "url": "/first-react-appgit/index.html"
+  },
+  {
+    "revision": "9dd3fb04a825a448c7f4",
+    "url": "/first-react-appgit/static/css/main.20796928.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/first-react-appgit/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/first-react-appgit/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/first-react-appgit/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "9dd3fb04a825a448c7f4",
+    "url": "/first-react-appgit/static/js/main.132a2354.chunk.js"
+  },
+  {
+    "revision": "d333d2bc1bc671c94003",
+    "url": "/first-react-appgit/static/js/runtime-main.202ed48b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/first-react-appgit/static/media/logo.06e73328.svg"
+  }
+]);
