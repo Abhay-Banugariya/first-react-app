4585c13fc829ff1da172cbb0e831bab484481a32
Abhay Banugariya <abhaybanugariya09@gmail.com>
2024-02-12 17:58:24 +0530

    Updates

---
+++ precache-manifest.aff8b8ba4dbc448f9e2cc62fd745ab12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ec41dada598002b44f41184e06d8ca",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "cf6881caafb79054fac2",
+    "url": "/first-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f024d7554546df4b4f5",
+    "url": "/first-react-app/static/js/2.851dbd97.chunk.js"
+  },
+  {
+    "revision": "0f431788df9fedcd7536120fa5bb2b3b",
+    "url": "/first-react-app/static/js/2.851dbd97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbf512de136ab4d3d82",
+    "url": "/first-react-app/static/js/3.54f2bb70.chunk.js"
+  },
+  {
+    "revision": "cf6881caafb79054fac2",
+    "url": "/first-react-app/static/js/main.ca31753f.chunk.js"
+  },
+  {
+    "revision": "865acd101c7768b2d800",
+    "url": "/first-react-app/static/js/runtime-main.cd555943.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86e328892f1b939ec1c92dc5b5760c95.js"
+  "/first-react-app/precache-manifest.aff8b8ba4dbc448f9e2cc62fd745ab12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/first-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
